07b98555251ab5f9252b280a65bf38ccff2d0a7b
Alex Sherman <asherman.ca@gmail.com>
2024-12-30 10:13:31 -0800

    crop tool

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,7 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
-import { UploadThingError } from 'uploadthing/server'
+import sharp from 'sharp'
 import { z } from 'zod'
+import { db } from '@/db'
 
 const f = createUploadthing()
 
@@ -18,10 +19,35 @@ export const ourFileRouter = {
 		.onUploadComplete(async ({ metadata, file }) => {
 			// This code RUNS ON YOUR SERVER after upload
 			// console.log('Upload complete for userId:', metadata.userId)
-
 			const { configId } = metadata.input
 
-			return { configId }
+			const res = await fetch(file.url)
+			const buffer = await res.arrayBuffer()
+			const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
+
+			if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        })
+
+        return { configId: configuration.id }
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        })
+
+        return { configId: updatedConfiguration.id }
+      }
 		}),
 } satisfies FileRouter
 
